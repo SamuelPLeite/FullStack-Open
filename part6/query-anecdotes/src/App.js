23aa595c0ceb375ed1f0880352d45890b84276a3
Samuel Leite <shpl19@inf.ufpr.br>
2023-04-05 16:57:24 -0300

    Exercises 6.23.-6.24.

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -2,9 +2,11 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { useQuery, useMutation, useQueryClient } from 'react-query'
 import { getAnecdotes, updateAnecdote } from './requests'
+import { useNotificationDispatch } from './NotificationContext'
 
 const App = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
 
   const result = useQuery('anecdotes', getAnecdotes,
     {
@@ -18,6 +20,11 @@ const App = () => {
 
   const handleVote = (anecdote) => {
     updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+
+    dispatch({ type: 'SET', payload: `Voted for '${anecdote.content}' anecdote.` })
+    setTimeout(() => {
+      dispatch({ type: 'REMOVE' })
+    }, 5000)
   }
 
   if (result.isLoading) {
--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,13 +1,22 @@
 import { useMutation, useQueryClient } from "react-query"
 import { createAnecdote } from "../requests"
+import { useNotificationDispatch } from "../NotificationContext"
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
 
   const newAnecdoteMutation = useMutation(createAnecdote, {
     onSuccess: () => {
       queryClient.invalidateQueries('anecdotes')
     },
+    onError: (err) => {
+      const error = err.response.data.error
+      dispatch({ type: 'SET', payload: error })
+      setTimeout(() => {
+        dispatch({ type: 'REMOVE' })
+      }, 5000)
+    }
   })
 
   const onCreate = (event) => {
@@ -17,6 +26,11 @@ const AnecdoteForm = () => {
     event.target.anecdote.value = ''
 
     newAnecdoteMutation.mutate({ content, votes: 0 })
+
+    dispatch({ type: 'SET', payload: `Added anecdote '${content}' successfully.` })
+    setTimeout(() => {
+      dispatch({ type: 'REMOVE' })
+    }, 5000)
   }
 
   return (
